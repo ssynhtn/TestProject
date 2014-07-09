23f0df241a4e202ed11adb2511f15dae7340f366
ssynhtn <ssynhtn@gmail.com>
2014-07-09 19:47:32 +0800

    add some println

--- com/ssynhtn/Test.java
+++ com/ssynhtn/Test.java
@@ -4,5 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is a test");
 		System.out.println("Hello world!");
+
+		System.out.println("This is from an ssh clone");
 	}
 } 
